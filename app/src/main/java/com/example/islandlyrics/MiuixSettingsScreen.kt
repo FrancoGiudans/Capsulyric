2f0dc938b5c15e3a91e094f9d89564cc98cf42f7
Franco <franklinsmithson@outlook.com>
2026-02-25 02:23:53 +0800

    fix: compile issue

--- app/src/main/java/com/example/islandlyrics/MiuixSettingsScreen.kt
+++ app/src/main/java/com/example/islandlyrics/MiuixSettingsScreen.kt
@@ -20,10 +20,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import top.yukonga.miuix.kmp.basic.*
