bc309ed7bb23819bc35cfdc16186a88ad20483ad
Franco <franklinsmithson@outlook.com>
2026-07-11 15:47:03 +0800

    update copyright

--- app/src/main/java/com/example/islandlyrics/runtime/media/ProgressSyncController.kt
+++ app/src/main/java/com/example/islandlyrics/runtime/media/ProgressSyncController.kt
@@ -1,3 +1,25 @@
+/*
+ *
+ *  * Copyright (c) 2026 Franco Giudance
+ *  *
+ *  * This file is part of Capsulyric.
+ *  *
+ *  * Capsulyric is free software: you can redistribute it and/or modify
+ *  * it under the terms of the GNU General Public License as published by
+ *  * the Free Software Foundation, either version 3 of the License, or
+ *  * (at your option) any later version.
+ *  *
+ *  * Capsulyric is distributed in the hope that it will be useful,
+ *  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  * GNU General Public License for more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License
+ *  * along with Capsulyric. If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package com.example.islandlyrics.runtime.media
 
 import android.content.ComponentName
